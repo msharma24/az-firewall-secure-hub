fc338a56b1909321fd3e33d2ee781a00aeb2ba0e
Mukesh Sharma <mukesh.sharma@airnz.co.nz>
2024-03-11 12:34:50 +1300

    base config

---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "random_pet" "random_pet" {
+  length    = 2
+  separator = "-"
+}
+
+locals {
+  prefix = "${var.env}-${random_pet.random_pet.id}"
+}
+
+#------------------------------------------------    
+# Resource Group                                     
+# ------------------------------------------------   
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${local.prefix}-rg"
+  location = "australiaeast"
+
+}
+
+#------------------------------------------------
+# Virtual WAN
+# ------------------------------------------------
+resource "azurerm_virtual_wan" "virtual_wan" {
+  name                = "${local.prefix}-vw"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+}
+
+#------------------------------------------------    
+# Virtual Hub
+#------------------------------------------------    
+resource "azurerm_virtual_hub" "virtual_hub" {
+  name                = "${local.prefix}-vh"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
+  address_prefix      = "10.0.0.0/23" #"10.0.0.0/16"
+  sku                 = "Standard"
+
+}
+
+#------------------------------------------------
+# Azure Firewall 
+# ------------------------------------------------
+resource "azurerm_firewall" "firewall" {
+  name                = "${local.prefix}-fw"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku_tier            = "Standard"
+  sku_name            = "AZFW_Hub"
+
+  virtual_hub {
+    virtual_hub_id = azurerm_virtual_hub.virtual_hub.id
+  }
+
+
+}
---
+++ output.tf
@@ -0,0 +1,38 @@
+output "random_pet_name" {
+  value = random_pet.random_pet.id
+}
+
+
+output "resource_group_location" {
+  value = azurerm_resource_group.resource_group.location
+}
+
+
+output "resource_group_name" {
+  value = azurerm_resource_group.resource_group.name
+
+}
+
+output "virtual_hub_name" {
+  value = azurerm_virtual_hub.virtual_hub.name
+}
+
+output "virtual_wan_name" {
+  value = azurerm_virtual_wan.virtual_wan.name
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
